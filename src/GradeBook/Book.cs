b9762631ab3e1b2c5ed1a6e68652ae91ed8ca99d
ccraddo <45082319+ccraddo@users.noreply.github.com>
2020-09-20 13:49:27 -0400

    book class

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,19 @@
+  namespace GradeBook  {
+    using System.Collections.Generic;
+    class Book{
+        private List<double> grades; 
+
+        private string name;
+        public Book(string name){
+            this.name = name;
+            grades = new List<double>(); 
+        }
+        public void AddGrade(double grade){
+            grades.Add(grade);
+
+        }
+
+    }
+
+
+  }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book{
-
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            // further change 2
+            // further change
 
-            var book = new Book();
+            var book = new Book("Colin's book");
+
+            book.AddGrade(89.5);
+
+            System.Console.WriteLine("");
 
             List<double> grades = new List<double>(){ 80.1,79,100,92.5};
             double result = 0;
