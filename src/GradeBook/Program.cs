05366c729d7f2b45c836ce1e8bbb940ef3a94323
ccraddo <45082319+ccraddo@users.noreply.github.com>
2020-09-24 07:21:20 -0400

    commited test

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,23 +12,9 @@ namespace GradeBook
             var book = new Book("Colin's book");
             
             book.AddGrade(89.5);
-
-            System.Console.WriteLine("");
-
-            List<double> grades = new List<double>(){ 80.1,79,100,92.5};
-           
+        
             book.ShowStatistics();
            
-            double result = 0;
-
-            foreach (var grade in grades)
-            {
-                result += grade;    
-            }
-
-            double avg = result/ grades.Count;
-
-            System.Console.WriteLine($"Average {avg:N2}");
         }
     }
 }
